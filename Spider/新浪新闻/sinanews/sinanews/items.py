3232bad9c17c5030a85a22068639d911450aaa52
pol9111 <biscuit36@163.com>
2018-07-09 17:09:41 +0800

    exercise

---
+++ Spider/新浪新闻/sinanews/sinanews/items.py
@@ -0,0 +1,24 @@
+# items.py
+
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+class SinaItem(scrapy.Item):
+    # 大类的标题 和 url
+    parentTitle = scrapy.Field()
+    parentUrls = scrapy.Field()
+
+    # 小类的标题 和 子url
+    subTitle = scrapy.Field()
+    subUrls = scrapy.Field()
+
+    # 小类目录存储路径
+    # subFilename = scrapy.Field()
+
+    # 小类下的子链接
+    sonUrls = scrapy.Field()
+
+    # 文章标题和内容
+    head = scrapy.Field()
+    content = scrapy.Field()
